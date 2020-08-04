1fb84f3eff28e345b8e5470c9839e3532013be64
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-08-04 11:17:10 -0500

    restructred app layout

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// css imports
+import "../assets/style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>App Component</div>
+    </div>
+  );
+}
+
+export default App;
