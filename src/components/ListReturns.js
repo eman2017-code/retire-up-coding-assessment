9b7b9dff13c125d1911d0c888648c2b119e0e0c8
Emmanuel Miller <emmanuelmiller355@gmail.com>
2020-08-05 16:09:04 -0500

    created method to render total returns/years in asc order

--- src/components/ListReturns.js
+++ src/components/ListReturns.js
@@ -13,75 +13,90 @@ class ListReturns extends React.Component {
     this.state = {
       records: [],
       cumulativeReturnArr: [],
+      // headerNames: ["Year", "TotalReturn", ""]
     };
   }
 
   componentDidMount() {
     // set the records in ascending order
     returns.reverse();
+
+    this.addCumulativeValues();
     this.createCumulativeKey();
-    this.addCumulativeValue();
   }
 
   // iterates through the json object and creates
   // a cumulativeKey key:value within each object
-  createCumulativeKey() {
-    returns.map(function (record) {
-      record.cumulativeKey = "";
+  createCumulativeKey = () => {
+    const cumulativeValuesArr = this.state.cumulativeReturnArr;
+
+    // // iterate through cumulativeReturnArr to grab each value
+    // for (let i = 0; i < cumulativeValuesArr.length; i++) {
+    //   // iterate through the returns array
+    //   console.log("cumulativeValuesArr[i]", cumulativeValuesArr[i]);
+    // }
+
+    returns.map((record) => {
+      record.cumulativeKey = "num";
       return record;
     });
-  }
+
+    this.setState({ records: returns });
+  };
 
   // sets the cumulative values for each record
   // in the returns array
-  addCumulativeValue() {
+  addCumulativeValues = () => {
+    // list for totalReturns
     const totalReturnArr = [];
+
+    // list for cumulative values
     const arr = [];
 
+    // convert each totalReturn into a number
     returns.forEach((el) => {
-      // convert each totalReturn into a number
       totalReturnArr.push(Number(el.totalReturn));
     });
 
     // create cumulative list of cumulative values
     totalReturnArr.reduce(function (a, b, i) {
-      return (arr[i] = a + b);
+      return (arr[i] = Math.round(a + b * 100) / 100);
     }, 0);
 
-    this.state.cumulativeReturnArr.push(arr);
-  }
+    // set cumulative returns array
+    this.setState({ cumulativeReturnArr: arr });
+    // this.state.cumulativeReturnArr.push(arr);
+  };
 
-  //   renderTableHeader() {
-  //     let header = Object.keys(this.state.records[0]);
-  //     console.log(header);
-  //     return header.map((key, i) => {
-  //       return <th key={i}>{key.toUpperCase()}</th>;
-  //     });
-  //   }
-
-  //   renderRow() {
-  //     return this.state.records.map((record, i) => {
-  //       const { year, totalReturn, cumulativeReturn } = record;
-
-  //       return (
-  //         <tr key={i}>
-  //           <td>{year}</td>
-  //           <td>{totalReturn}</td>
-  //           <td>{cumulativeReturn}</td>
-  //         </tr>
-  //       );
-  //     });
-  //   }
+  // renders elements out of records
+  renderRecordsData = () => {
+    return this.state.records.map((record, i) => {
+      const { year, totalReturn, cumulativeKey } = record;
+      return (
+        <tr key={i}>
+          <td>{year}</td>
+          <td>{totalReturn}</td>
+          <td>{cumulativeKey}</td>
+        </tr>
+      );
+    });
+  };
 
   render() {
+    // console.log("this.state in render", this.state);
     return (
       <div>
-        {/* <table id="returns">
-          <tbody>
-            <tr>{this.renderTableHeader()}</tr>
-            {this.renderRow()}
-          </tbody>
-        </table> */}
+        <h4>Slider Component will be going here</h4>
+        <table id="returns">
+          <thead>
+            <tr>
+              <th>Year</th>
+              <th>Total Return</th>
+              <th>Cumulative Return</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderRecordsData()}</tbody>
+        </table>
       </div>
     );
   }
